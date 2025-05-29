ea4991927e1cc88e5d055a6cbf13a3fa9b29908a
miqdad bada <mujaidbada@gmail.com>
2025-05-29 15:35:28 +0100

    head

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../components/Footer.css'
+
+
+const footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default footer
---
+++ src/components/Head.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../components/Head.css'
+
+const Head = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Head
